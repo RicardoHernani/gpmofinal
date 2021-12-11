1fffa60d72ff914b20373fe72c10b661a076979f
RicardoHernani <ricardohernani@yahoo.com.br>
2021-12-11 18:39:50 -0300

    Projeto criado

---
+++ src/main/java/com/ricardochaves/GpmofinalApplication.java
@@ -0,0 +1,13 @@
+package com.ricardochaves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpmofinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpmofinalApplication.class, args);
+	}
+
+}
