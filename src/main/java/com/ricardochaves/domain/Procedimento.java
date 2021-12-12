105316a13cf39694c38b6764c6ea78a7e3a932e9
RicardoHernani <ricardohernani@yahoo.com.br>
2021-12-11 21:17:10 -0300

    Criacao das classes cirurgia e procedimento com mapeamentos

---
+++ src/main/java/com/ricardochaves/domain/Procedimento.java
@@ -0,0 +1,105 @@
+package com.ricardochaves.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Procedimento implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	
+	private Integer tipo;
+	private Integer premio;
+	private Integer codigo;
+	
+	@ManyToOne
+	private Cirurgia cirurgia;
+	
+	public Procedimento() {
+	}
+
+	public Procedimento(Integer id, Integer tipo, Integer premio, Integer codigo, Cirurgia cirurgia) {
+		super();
+		this.id = id;
+		this.tipo = tipo;
+		this.premio = premio;
+		this.codigo = codigo;
+		this.cirurgia = cirurgia;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(Integer tipo) {
+		this.tipo = tipo;
+	}
+
+	public Integer getPremio() {
+		return premio;
+	}
+
+	public void setPremio(Integer premio) {
+		this.premio = premio;
+	}
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+
+	public Cirurgia getCirurgia() {
+		return cirurgia;
+	}
+
+	public void setCirurgia(Cirurgia cirurgia) {
+		this.cirurgia = cirurgia;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Procedimento other = (Procedimento) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO USUARIO(nome, email, senha) VALUES('Ricardo', 'ricardo@gmail', '12345');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Débora', 'debora@gmail', '123');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Ana', 'ana@gmail', '12345678');
