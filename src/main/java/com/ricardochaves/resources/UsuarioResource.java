48903a062ddbbdb4389c4500d471e0d4c6ab28ba
RicardoHernani <ricardohernani@yahoo.com.br>
2021-12-12 15:05:32 -0300

    Busca usuario por id e formatacao de objeto nao encontrado

--- src/main/java/com/ricardochaves/resources/UsuarioResource.java
+++ src/main/java/com/ricardochaves/resources/UsuarioResource.java
@@ -1,10 +1,27 @@
 package com.ricardochaves.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ricardochaves.domain.Usuario;
+import com.ricardochaves.dto.UsuarioDTO;
+import com.ricardochaves.services.UsuarioService;
 
 @RestController
 @RequestMapping(value="/usuarios")
 public class UsuarioResource {
 
+	@Autowired
+	private UsuarioService usuarioService;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<UsuarioDTO> findById(@PathVariable Integer id) {
+		Usuario obj = usuarioService.findById(id);
+		return ResponseEntity.ok().body(new UsuarioDTO(obj)); 
+	}
+	
 }
--- src/main/java/com/ricardochaves/services/UsuarioService.java
+++ src/main/java/com/ricardochaves/services/UsuarioService.java
@@ -1,9 +1,26 @@
 package com.ricardochaves.services;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.ricardochaves.domain.Usuario;
+import com.ricardochaves.repositories.UsuarioRepository;
+import com.ricardochaves.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UsuarioService {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 
-
+	public Usuario findById(Integer id) {
+		Optional<Usuario> obj = usuarioRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado! id: " + id
+				+ ", Tipo: " + Usuario.class.getName()));
+	}
+	
+	
+	
 }
