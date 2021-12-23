e5461ac1d872091d0d2942b2914d2b7ba5d94056
RicardoHernani <ricardohernani@yahoo.com.br>
2021-12-23 16:50:06 -0300

    Pesquisa da descricao na Referencia, paginado

--- src/main/java/com/ricardochaves/resources/ReferenciaResource.java
+++ src/main/java/com/ricardochaves/resources/ReferenciaResource.java
@@ -1,12 +1,15 @@
 package com.ricardochaves.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.ricardochaves.domain.Referencia;
+import com.ricardochaves.resources.utils.URL;
 import com.ricardochaves.services.ReferenciaService;
 
 @RestController
@@ -22,6 +25,16 @@ public class ReferenciaResource {
 		return ResponseEntity.ok().body(obj); 
 	}
 	
-	
+	@RequestMapping(value="/page", method=RequestMethod.GET)
+	public ResponseEntity<Page<Referencia>> findPage(
+			@RequestParam(value="text", defaultValue="") String text, 
+			@RequestParam(value="page", defaultValue="0") Integer page,
+			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage,
+			@RequestParam(value="orderBy", defaultValue="descricao") String orderBy,
+			@RequestParam(value="direction", defaultValue="ASC") String direction) {
+			text = URL.decodeParam(text);
+			Page<Referencia> list = referenciaService.findPage(text, page, linesPerPage, orderBy, direction);
+			return ResponseEntity.ok().body(list);
+	}
 	
 }
--- src/main/java/com/ricardochaves/services/ReferenciaService.java
+++ src/main/java/com/ricardochaves/services/ReferenciaService.java
@@ -1,8 +1,10 @@
 package com.ricardochaves.services;
 
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import com.ricardochaves.domain.Referencia;
 import com.ricardochaves.repositories.ReferenciaRepository;
@@ -20,5 +22,9 @@ public class ReferenciaService {
 				+ ", Tipo: " + Referencia.class.getName()));
 	}
 	
+	public Page<Referencia> findPage(String text, Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return referenciaRepository.findByDescricaoContainingIgnoreCase(text, pageRequest);
+	}
 	
 }
