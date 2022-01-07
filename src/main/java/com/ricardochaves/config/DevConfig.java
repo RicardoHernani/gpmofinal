be9fd9a05a82c00a5b99d71435f22aafa491518b
RicardoHernani <ricardohernani@yahoo.com.br>
2022-01-07 00:49:17 -0300

    Ajustes no perfil dev

--- src/main/java/com/ricardochaves/config/DevConfig.java
+++ src/main/java/com/ricardochaves/config/DevConfig.java
@@ -1,11 +1,23 @@
 package com.ricardochaves.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
+import com.ricardochaves.services.DBService;
 
 @Configuration
 @Profile("dev")
 public class DevConfig {
 	
-
+	@Autowired
+	private DBService dbService;
+		
+	@Bean
+	public boolean instantiateDatabase() {
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+	
 }
