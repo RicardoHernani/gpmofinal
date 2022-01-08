fbbb7f0ea1b450b49d69f87de0b548a5a2c70302
RicardoHernani <ricardohernani@yahoo.com.br>
2022-01-08 15:10:43 -0300

    Implementando autorizacao

--- src/main/java/com/ricardochaves/config/SecurityConfig.java
+++ src/main/java/com/ricardochaves/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.ricardochaves.security.JWTAuthenticationFilter;
+import com.ricardochaves.security.JWTAuthorizationFilter;
 import com.ricardochaves.security.JWTUtil;
 
 
@@ -60,6 +61,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDatailsService));
+
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
